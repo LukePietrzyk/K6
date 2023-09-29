7c4dbc2c33e1acbd37643c1936a873d7bddcca4e
Łukasz Pietrzyk <lukasz.pietrzyk97@gmail.com>
2023-09-29 19:12:58 +0200

    Modify file and add dynamic variable from test data from json file

--- T008K6_TC.js
+++ T008K6_TC.js
@@ -1,11 +1,15 @@
 import { check } from "k6";
 import http from "k6/http";
 import { sleep } from "k6";
-import { SharedArray } from "k6/data";
 
-const data = new SharedArray("data", function () {
-  return JSON.parse(open("data.json")).items;
-});
+const jsonData = JSON.parse(open("data.json"));
+
+function randomGenerator(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+const products = jsonData.items[randomGenerator(jsonData.items)];
+const users = jsonData.users[randomGenerator(jsonData.users)];
 
 export const options = {
   vus: 1,
@@ -30,19 +34,30 @@ export default function () {
       "Content-Type": "application/json",
     },
   };
-  const products = data[0];
+
   const res = http.post(urlLogin, payload, params);
   check(res, {
-    "Login - Status code is 200": (r) => r.status === 200,
-    "Response body has username": (r) =>
-      r.body.includes("kminchelle", "0lelplR"),
+    "Login - Status code is 200": (r) => {
+      return r.status === 200;
+    },
+    [`Response body has username ${users.username}`]: (r) => {
+      const jsonResponse = JSON.parse(r.body);
+      return jsonResponse.username === users.username;
+    },
+
+    [`is authenticated as user id = ${users.id}`]: (r) => {
+      const jsonResponse = JSON.parse(r.body);
+      return jsonResponse.id === users.id;
+    },
   });
 
   sleep(1);
   const res1 = http.get(urlProducts);
   check(res1, {
     "Get list of products - status code is 200": (r) => r.status === 200,
-    "Include iPhone in list of products": (r) =>
-      r.body.includes(products.title),
+    [`Include ${products.title} in list of products`]: (r) => {
+      const jsonResponse = JSON.parse(r.body);
+      return jsonResponse.products.some((ele) => ele.title === products.title);
+    },
   });
 }
