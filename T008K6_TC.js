56f3218242a8956b38310568d0bc12f2ee41382a
Łukasz Pietrzyk <lukasz.pietrzyk97@gmail.com>
2023-10-04 11:13:29 +0200

    Update files: T008K6 and json.file

--- T008K6_TC.js
+++ T008K6_TC.js
@@ -25,18 +25,30 @@ export const options = {
 export default function () {
   const urlLogin = "https://dummyjson.com/auth/login";
   const urlProducts = "https://dummyjson.com/products";
+  const urlSingleProduct = `https://dummyjson.com/products/${products.id}`;
+  const urlAddProduct = `https://dummyjson.com/products/add`;
+  const urlUpdateProduct = "https://dummyjson.com/products/1";
+  const productName = jsonData.products[randomGenerator(jsonData.products)];
+
+  console.log(productName);
+  //const productName = "BMW Pencil";
   const payload = JSON.stringify({
     username: "kminchelle",
     password: "0lelplR",
+    title: `${productName}`,
   });
   const params = {
     headers: {
       "Content-Type": "application/json",
     },
   };
+  const resLogin = http.post(urlLogin, payload, params);
+  const resAllProducts = http.get(urlProducts);
+  const resSinglePro = http.get(urlSingleProduct);
+  const resAddProduct = http.post(urlAddProduct, payload, params);
+  const updateProduct = http.put(urlUpdateProduct, payload, params);
 
-  const res = http.post(urlLogin, payload, params);
-  check(res, {
+  check(resLogin, {
     "Login - Status code is 200": (r) => {
       return r.status === 200;
     },
@@ -52,12 +64,34 @@ export default function () {
   });
 
   sleep(1);
-  const res1 = http.get(urlProducts);
-  check(res1, {
+  check(resAllProducts, {
     "Get list of products - status code is 200": (r) => r.status === 200,
     [`Include ${products.title} in list of products`]: (r) => {
       const jsonResponse = JSON.parse(r.body);
       return jsonResponse.products.some((ele) => ele.title === products.title);
+    },
+  });
+  sleep(1);
+  check(resSinglePro, {
+    "Get single product - status code 200": (r) => r.status === 200,
+    [`Check product ${products.title}`]: (r) => {
+      const jsonRes = JSON.parse(r.body);
+      return jsonRes.title === products.title;
+    },
+  });
+  sleep(1);
+  check(resAddProduct, {
+    "Status is 200": (r) => r.status === 200,
+    [`Add product ${productName}`]: (r) => {
+      const resAdd = JSON.parse(r.body);
+      return resAdd.title === productName;
+    },
+  });
+  sleep(1);
+  check(updateProduct, {
+    [`Update product name to (${productName}) with id = 1 `]: (r) => {
+      const resUpdate = JSON.parse(r.body);
+      return resUpdate.title === productName;
     },
   });
 }
